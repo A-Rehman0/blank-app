47e13782658850b54d9da9082e9e46ef05768879
A-Rehman0 <96643540+A-Rehman0@users.noreply.github.com>
2025-03-11 17:43:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,24 @@
+import pickle
+import pandas as pd
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+sih = pickle.load(open("hard_sih.pkl","rb"))
+
+st.title("My new app")
+selecting = st.selectbox("Select Your Poblem Statement",sih['Statement_id'])
+Butt = st.button("Next")
+# for i in selecting:
+# for i in range(0,67):
+if selecting:
+  index=sih[sih['Statement_id']==selecting].index[0]
+  info = sih.iloc[index]
+  st.table(info)
+ 
+
+# for i in range(0,67):
+#  if Butt:
+#     index=sih[sih['Statement_id']==selecting].index[0]
+#     info = sih.iloc[index+i]
+#     st.table(info)
+st.write("List")
+st.data_editor(sih)
